4ef24563094206e4e3fcd5c9d08851e84705b3a7
yyogesh <yogeshggn@hotmail.com>
2024-04-28 10:27:01 +0530

    job controller

--- src/main/java/com/example/JobSpringApplication/controller/JobController.java
+++ src/main/java/com/example/JobSpringApplication/controller/JobController.java
@@ -1,12 +1,20 @@
 package com.example.JobSpringApplication.controller;
 
 import com.example.JobSpringApplication.model.JobPost;
+import com.example.JobSpringApplication.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class JobController {
+
+    @Autowired
+    private JobService jobService;
 
     @GetMapping({"/", "home"})
     public String home() {
@@ -21,7 +29,15 @@ public class JobController {
     @PostMapping("handleForm")
     public String handleForm(JobPost jobPost) {
         System.out.println("jobPost" + jobPost);
+        jobService.addJobRequest(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model model) {
+        List<JobPost> jobPosts = jobService.returnAllJobPosts();
+        model.addAttribute("jobPosts", jobPosts);
+        return "viewalljobs";
     }
 
 
--- src/main/java/com/example/JobSpringApplication/service/JobService.java
+++ src/main/java/com/example/JobSpringApplication/service/JobService.java
@@ -1,2 +1,23 @@
-package com.example.JobSpringApplication.service;public class JobService {
+package com.example.JobSpringApplication.service;
+
+import com.example.JobSpringApplication.model.JobPost;
+import com.example.JobSpringApplication.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public List<JobPost> returnAllJobPosts() {
+        return jobRepository.returnAllJobPosts();
+    }
+
+    public void addJobRequest(JobPost jobPost) {
+        jobRepository.addJobPost(jobPost);
+    }
 }
